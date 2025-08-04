0d6b5001e373d9756e7772053b4cbff0db3a267c
Gargantua7 <1505011271@qq.com>
2025-08-04 10:07:42 +0800

    Reproduce Tencent-TDS/KuiklyBase-kotlin#9

---
+++ composeApp/src/ohosArm64Main/kotlin/com/tencent/compose/Clickable.kt
@@ -0,0 +1,41 @@
+package com.tencent.compose
+
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.IndicationInstance
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+
+object DefaultIndication : Indication {
+
+    private class DefaultIndicationInstance(
+        private val isPressed: State<Boolean>,
+        private val isHovered: State<Boolean>,
+        private val isFocused: State<Boolean>,
+    ) : IndicationInstance {
+        override fun ContentDrawScope.drawIndication() {
+            drawContent()
+            if (isPressed.value) {
+                drawRect(color = Color.Black.copy(alpha = 0.05f), size = size)
+            } else if (isHovered.value || isFocused.value) {
+                drawRect(color = Color.Black.copy(alpha = 0.01f), size = size)
+            }
+        }
+    }
+
+    @Composable
+    override fun rememberUpdatedInstance(interactionSource: InteractionSource): IndicationInstance {
+        val isPressed = interactionSource.collectIsPressedAsState()
+        val isHovered = interactionSource.collectIsHoveredAsState()
+        val isFocused = interactionSource.collectIsFocusedAsState()
+        return remember(interactionSource) {
+            DefaultIndicationInstance(isPressed, isHovered, isFocused)
+        }
+    }
+}
